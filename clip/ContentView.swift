f2682ac5f54d9bc7083dd2afe0333c145a822c5a
Xu_mj <331538695@qq.com>
2026-05-21 18:37:40 +0800

    Initial Commit

---
+++ clip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clip
+//
+//  Created by 许梦杰 on 2026/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ clip/clipApp.swift
@@ -0,0 +1,17 @@
+//
+//  clipApp.swift
+//  clip
+//
+//  Created by 许梦杰 on 2026/5/21.
+//
+
+import SwiftUI
+
+@main
+struct clipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
